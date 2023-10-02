51ef1331da151ca31c2539ab9bca7868dbb3c5ae
Vishal Madhav <57302592+vm0303@users.noreply.github.com>
2023-10-02 16:25:45 -0400

    Changed the welcome message in App.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello!'));
+app.get('/', (req, res) => res.send('Goal:'));
+
+app.get('/', (req, res) => res.send('Set up a Git repo, deploy a sample web app, and create a continuous delivery pipeline.'));
+
+
+
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
