8ae7307591f624f0581f8f7ae0f645c56846e1c7
Vishal Madhav <57302592+vm0303@users.noreply.github.com>
2023-10-02 16:38:16 -0400

    Rearranges the sentence structure Added some more extra words for the welcome message in app.js. This is the final change.

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Hello!'));
-app.get('/', (req, res) => res.send('Goal for this project:'));
 
-app.get('/', (req, res) => res.send('Set up a Git repo, deploy a sample web app, and create a continuous delivery pipeline.'));
-
-
-
+app.get('/', (req, res) => res.send('This is an AWS Hands-On project where we create a' +
+    ' continuous delivery pipeline by setting up a GitHub repository, deploying a sample web application, and ' +
+    'creating a continuous delivery pipeline. '));
 
 app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+console.log(`The app is running on http://localhost:${port}`);
