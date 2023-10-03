a5a403946ae088a75d934512e910994ff2dfaf58
Vishal Madhav <57302592+vm0303@users.noreply.github.com>
2023-10-02 23:23:14 -0400

    Changing the message in Line 5 to something different in App.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello!'));
+app.get('/', (req, res) => res.send('This project from AWS is really cool!'));
 
 app.get('/', (req, res) => res.send('This is an AWS Hands-On project where we create a' +
     ' continuous delivery pipeline by setting up a GitHub repository, deploying a sample web application, and ' +
